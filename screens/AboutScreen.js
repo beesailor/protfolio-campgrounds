40f8b6adf3a98f94720e5cc82ac19e4acb0bf6bb
BaronE <besailor@protonmail.com>
2022-11-06 19:09:04 -0600

    rn w1 t3 in progress

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,13 +1,32 @@
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { PARTNERS } from '../shared/partners';
+
+const Mission = () => {
+    
+    return (
+        <Card>
+            <CardTitle>
+                Our Mission
+            </CardTitle>
+            <CardDivider/>
+            <Text style = {{ margin:10 }}>
+            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
+    )
+}
 
 const AboutScreen = () => {
+    const [partners, setPartners] = useState(PARTNERS);
     
-
-    return (
+    const
+    
+    //return (
         <ScrollView>
             
         </ScrollView>
-    );
+    //);
 };
 
 export default AboutScreen
