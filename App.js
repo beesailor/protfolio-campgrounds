9ec9b2e9353531feed62c918de0b563cddd754c0
BaronE <besailor@protonmail.com>
2022-11-12 10:27:36 -0600

    removed unused line

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-//import { store } from './redux/store';
 import { Store } from 'redux/store';
 
 export default function App() {
