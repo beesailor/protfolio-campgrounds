e392528061d73fc10bc95547e6ce2250f009e124
BaronE <besailor@protonmail.com>
2022-11-15 00:36:38 -0600

    w2 workship video all but text strings fixed

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -3,64 +3,58 @@ import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
 import { useState } from 'react';
-import { Rating, Author, Comment } from 'react-native-elements';
+import { Rating, Input, Author, Comment } from 'react-native-elements';
 import { postComment } from '../features/comments/commentsSlice';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
-    const dispatch = useDispatch();
-    
-//TASK ONE STEP: CREATE A LOCAL STATE VARIABLE CALLED SHOW MODAL
     const [showModal, setShowModal] = useState(false);
-// TASK TWO STEP: INTITIALIZE THREE NEW STATE VARIABLES
-    const rating = 5;
-    const author = "";
-    const text = "";
+    const [rating, setRating] = ("5");
+    const [author, setAuthor] = (" ");
+    const [text, setText] = (" ");
 
-//TASK TWO: ADD A NEW FUNCTION- HANDLE SUBMIT
+    const dispatch = useDispatch();
+
     const handleSubmit = ()=> {
-        return (
-            newComment = {
-                author,
-                rating,
-                text,
-                campsiteId: campsite.id
-            }
-        );
-        dispatch(postComment(newComment));
+        newComment = {
+            author,
+            rating,
+            text,
+            campsiteId: campsite.id
+        }
+        dispatch(postComment(newComment))
+        setShowModal(!showModal)
     };
 
-//TASK TWO: ADD A NEW FUNCTION- RESET FORM    
     const resetForm = () => {
-        return (
-        setRating(5);
-        setAuthor("");
-        setTitle("");
-        );
+        setRating(5)
+        setAuthor("")
+        setTitle("")
     };
 
     const renderCommentItem = ({ item }) => {
-        const { rating } = this.props;
-        <Rating imageSize={10} readonly startingValue={this.rating} style={styles.rating} />;
-        
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
-                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Rating
+                    readonly
+                    startingValue={rating} 
+                    imageSize={10}
+                    style={{ alignItems: 'flex-start', paddingvertical: '5%'}}
+                />
                 <Text style={{ fontSize: 12 }}>
                     {`-- ${item.author}, ${item.date}`}
                 </Text>
             </View>
-            
         );
     };
 
+//good here down
     return (
         <>
             <FlatList
-                
                 data={comments.commentsArray.filter(
                     (comment) => comment.campsiteId === campsite.id
                 )}
@@ -76,71 +70,66 @@ const CampsiteInfoScreen = ({ route }) => {
                             campsite={campsite}
                             isFavorite={favorites.includes(campsite.id)}
                             markFavorite={() => dispatch(toggleFavorite(campsite.id))}
-                            
-//TASK ONE PASS THE EVENT HANDLER...
                             onShowModal={() => setShowModal(!showModal)}
                         />
                         <Text style={styles.commentsTitle}>Comments</Text>
                     </>
                 }
             />
-//TASK ONE ADD THE MODAL
             <Modal
-            animationType='slide'
-            transparent={false}
-            visible={showModal}
-            onRequestClose={() => setShowModal(!showModal)}
+                animationType='slide'
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}>
             >
                 <View style={styles.modal}>
-                    <Rating>
+                    <Rating
                         showRating
-                        {/* starting value should equal "the state" */}
-                        startingValue="{3}" 
+                        startingValue={rating} 
                         imageSize={40}
                         onFinishRating={(rating)=> setRating(rating)} 
                         style={{paddingVertical: 10}}
-                    </Rating>
+                    />
                     <Input
-                        placeholder='BASIC INPUT'
-                        leftIcon={{ type: 'font-awesome', name: 'user-o'}}
-                        leftIconContainerStyle
-                        onChangeText={(rating)=> setRating(rating)} 
-                        value={text}
-                    >
-                    </Input>
+                            placeholder='Author'
+                            leftIcon={{ type: 'font-awesome', name: 'user-o'}}
+                            leftIconContainerStyle
+                            onChangeText={(author)=> setAuthor(author)} 
+                            value={author}
+                    />
                     <Input
-                        placeholder='BASIC INPUT'
+                        placeholder='Comment'
                         leftIcon={{ type: 'font-awesome', name: 'comment-o'}}
                         leftIconContainerStyle
-                        onChangeText={(rating)=> setRating(rating)} 
+                        onChangeText={(text)=> setText(text)} 
                         value={text}
-                    >
-                    </Input>
+                    />
                     <View style={{margin: 10}}>
-                        <Button>
+                        <Button
                             onPress={()=> {
                                 handleSubmit();
                                 resetForm();
                             }}
                             title='Submit'
-                            color='#5637DD'
-                        </Button>
+                            color='563700'
+                        />
                     </View>
                     <View style={{margin: 10}}>
-                        <Button>
-                            onPress={() => {
+                        <Button
+                            onPress={()=> {
                                 setShowModal(!showModal);
                                 resetForm();
                             }}
-                            color='#808080'
                             title='Cancel'
-                        </Button>
+                            color='808080'
+                        />
                     </View>
                 </View>
-            </Modal> 
+            </Modal>
         </>
     );
 };
+    
 
 const styles = StyleSheet.create({
     CommentsTitle: {
@@ -157,7 +146,6 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         backgroundColor: '#fff'
     },
-//TASK ONE SET STYLE FOR MODAL    
     Modal: {
         justifyContent: 'center',
         margin: 20
